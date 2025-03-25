f0fe75fee78ce9042ca31c0afe89b9bb54762e96
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2025-03-25 15:55:47 +0500

    Update pass_check.py

--- pass_check.py
+++ pass_check.py
@@ -4,36 +4,39 @@ st.markdown("**🔐Password check**")
 st.write("Enter your password and check whether its weak, medium or strong.")
 
 special_characters= "!@#$%^&*()-_=+;:'/.>,<`~?"
-password_input= st.text_input("Enter Password")
+password_input= st.text_input("Enter Password", type="password")
 
 strength= 0
 
 if password_input:
     has_upper= any(char.isupper() for char in password_input)
+    has_lower=any(char.islower() for char in password_input)
     has_digit= any(char.isdigit() for char in password_input)
     has_special= any(char in special_characters for char in password_input)
-    
 
     if has_upper:
         strength+=1
     else:
-        st.write("⚠ Add a capital letter in your password")
+        st.write("⚠ Add atleast one capital letter in your password")
 
     if has_digit:
         strength+=1
     else:
-        st.write("⚠ Add a digit in your password")
+        st.write("⚠ Add digit in your password")
+
+    if not has_lower:
+        ("⚠ Add atleast one lower case letter in your password")    
 
     if has_special:
         strength+=1
     else:
-        st.write("⚠ Add a special character in your password")        
+        st.write("⚠ Add special character in your password")        
 
     if strength== 0:
         st.write("❌Your password is **weak**")
     elif strength== 1:
-        st.write("☑Your password **need improvement**")
+        st.write("❗Your password **need improvement**")
     elif strength== 2:
-        st.write("☑Your password is **medium**")  
+        st.write("❗Your password is **medium**")  
     elif strength== 3:
         st.write("✅Your password is **strong**")    
